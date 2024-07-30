77b3a82dbd6117b5f96b6e05693196ef3ee21711
Morricone Manreal <morriconemanreal@gmail.com>
2024-07-30 19:54:04 -0400

    fetch update but under router branch

--- src/pages/films.page.jsx
+++ src/pages/films.page.jsx
@@ -3,6 +3,7 @@ import "./FilmsPage.css";
 
 export default function FilmsPage() {
   const [movies, setMovies] = useState([]);
+  const [sortSelection, setSortSelection] = useState("releaseDate");
 
   useEffect(() => {
     fetch(`https://studioghibliapi-d6fc8.web.app/films`)
@@ -14,11 +15,57 @@ export default function FilmsPage() {
       .catch(console.error);
   }, []);
 
+  function sortMovies(films, sortType) {
+    switch (sortType) {
+      case "releaseDate":
+        return films.toSorted((a, b) => a.release_date - b.release_date);
+      case "director":
+        return films.toSorted((a, b) => {
+          if (a.director > b.director) {
+            return 1;
+          } else if (a.director < b.director) {
+            return -1;
+          } else {
+            return 0;
+          }
+        })
+      case "title":
+        return films.toSorted((a, b) => {
+          if (a.title > b.title) {
+            return 1;
+          } else if (a.title < b.title) {
+            return -1;
+          } else {
+            return 0;
+          }
+        })
+      case "score":
+        return films.toSorted((a, b) => b.rt_score - a.rt_score);
+    }
+  }
+
+  //derived State... sort = mutate the whole array, toSorted = sort the array and return a whole new sorted array
+  const sortedMovies = sortMovies(movies, sortSelection);
+
   return (
     <>
       <h1>Studio Ghibli Films</h1>
+      <label htmlFor="sortSelect">Sort by: </label>
+      <select
+        name="sortSelect"
+        id="sortSelect"
+        value={sortSelection}
+        onChange={(changeEvent) => {
+          setSortSelection(changeEvent.target.value);
+        }}
+      >
+        <option value="releaseDate"> Release Date</option>
+        <option value="director"> Director</option>
+        <option value="title"> Title</option>
+        <option value="score"> Rotten Tomatoes Score</option>
+      </select>
       <ul>
-        {movies.map((movie) => {
+        {sortedMovies.map((movie) => {
           return <li key={movie.id} className="movieCard">
             <h2>{movie.title}</h2>
             <div className="movieInfo">
